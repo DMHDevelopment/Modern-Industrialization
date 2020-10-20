a365940277756dafad6ae283051f9edf0ecb235a
Dmitrium Belich <belicdima9@gmail.com>
2020-10-20 21:25:46 +0700

    Ore(test)

---
+++ src/mod_gcc.java
@@ -0,0 +1,5 @@
+package net.minecraft.src;
+public class mod_gcc extends BaseMod
+{
+    public static Block AluminiumOre = new BlockNew(101, 0).setHardness(0.5F).setResistance(0.0F).setBlockName("AluminiumOre");
+}
